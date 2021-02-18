10926234b5250fb059483de0e022566a3f596883
fmandujano <fmandujano@maquivol.com>
2021-02-18 13:00:27 -0600

    Agrega Kotlin Xtensions, fragment para pantalla de contacto

--- app/src/main/java/com/sae/taller/ejemplo2/MainActivity.kt
+++ app/src/main/java/com/sae/taller/ejemplo2/MainActivity.kt
@@ -11,63 +11,9 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity()
 {
-    //Propiedades o variables
-    val contador:Int = 0
-    val estatura:Float = 1.89f
-    val nombre:String = "Lalo Landa"
-    val igURL:String = "http://www.instagram.com/lalo"
-    val githubURL:String = "https://github.com/paquito"
-    val emailURL:String = "mailto:f.mandujano@saei.mx"
-
-
-
-
-    //funciones o métodos
-    fun suma( A:Float, B:Float):Float
-    {
-        return A+B
-    }
-
-    fun cambiarNombre( nuevoNombre:String )
-    {
-        val textview:TextView = findViewById(R.id.titulo)
-        textview.text = nuevoNombre
-    }
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //comentario
-        //Log.d( "MiApp", "Resultado " + suma( 99.9f, 0.1f)     )
-        //cambiarNombre(nombre)
-
-        //referencia a los botones
-        //val btnIg:Button = findViewById(R.id.buttonIG)
-        //val btnEmail:Button = findViewById(R.id.buttonEmail)
-        //val btnGithub:Button = findViewById(R.id.buttonGithub)
-
-        //modificar el comportamiento al hacer click
-        /*
-        btnIg.setOnClickListener {
-            Log.d("MiApp", "Abriendo instagram del usuario")
-
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(igURL)))
-        }
-
-        btnEmail.setOnClickListener {
-            Log.d("MiApp", "Abriendo correo")
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(emailURL)))
-        }
-
-         */
-
-
-
-
     }
 }
--- app/src/main/java/com/sae/taller/ejemplo2/MainMenu.kt
+++ app/src/main/java/com/sae/taller/ejemplo2/MainMenu.kt
@@ -5,10 +5,15 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.main_menu.*
+import kotlinx.android.synthetic.main.main_menu.view.*
 
 /*
     Esto es el controlador de la pantalla del menú principal
@@ -19,4 +24,47 @@ class MainMenu : Fragment()
     {
         super.onCreate(savedInstanceState)
     }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val vista : View
+
+        vista = inflater.inflate(R.layout.main_menu, container, false)
+
+
+        //asignar la acción del botón
+        vista.btnLogros.setOnClickListener {
+            Log.d("MiApp", "clic o tap en boton de logros")
+            findNavController().navigate(R.id.action_mainMenu_to_logros)
+        }
+
+        vista.btnPortfolio.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenu_to_placeholder2)
+        }
+
+        vista.btnTrabajos.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenu_to_placeholder_trabajos)
+        }
+
+        vista.btnEducacion.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenu_to_placeholder3)
+        }
+
+        vista.btnContacto.setOnClickListener {
+            findNavController().navigate(R.id.action_mainMenu_to_contactFragment)
+        }
+
+        //asignar el layout a este controlador
+        // Inflate the layout for this fragment
+        return vista
+    }
+
+    fun suma( a: Float, b: Float ) : Float
+    {
+        return a + b
+    }
+
 }
