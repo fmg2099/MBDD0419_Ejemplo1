9f98f30d9f4b0c7c57c0d65d10a90538f7d379c0
fmandujano <fmandujano@maquivol.com>
2021-02-25 13:08:10 -0600

    Botones de contacto

--- app/src/main/java/com/sae/taller/ejemplo2/ContactFragment.kt
+++ app/src/main/java/com/sae/taller/ejemplo2/ContactFragment.kt
@@ -1,10 +1,14 @@
 package com.sae.taller.ejemplo2
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+//para importar las propiedades definidas en el xml
+import kotlinx.android.synthetic.main.fragment_contact.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +37,37 @@ class ContactFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val myView:View
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        myView = inflater.inflate(R.layout.fragment_contact, container, false)
+
+        myView.btnEmail.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse( resources.getString( R.string.contact_email) )))
+        }
+
+        myView.btnTwitter.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse( resources.getString( R.string.contact_tw) )))
+        }
+
+        myView.btnFabebook.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse( resources.getString( R.string.contact_fb) )))
+        }
+
+        myView.btnInstagram.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse( resources.getString( R.string.contact_ig) )))
+        }
+
+        myView.btnClubhouse.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                Uri.parse( resources.getString( R.string.contact_clubhouse) )))
+        }
+
+        return myView
     }
 
     companion object {
